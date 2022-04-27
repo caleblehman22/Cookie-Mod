384f1d8c5e80c997e01d03f8220434d16a6a1639
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 09:22:58 -0400

    added ore generation and fixed entity generation

--- src/main/java/com/lemon/cookieextras/CookieExtras.java
+++ src/main/java/com/lemon/cookieextras/CookieExtras.java
@@ -8,6 +8,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.entity.EntitySpawnPlacementRegistry;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -53,6 +57,10 @@ public class CookieExtras
 
     private void setup(final FMLCommonSetupEvent event)
     {
+        event.enqueueWork(() -> {
+            EntitySpawnPlacementRegistry.register(ModEntityTypes.COOKIE_MONSTER.get(), EntitySpawnPlacementRegistry.PlacementType.ON_GROUND,
+                    Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MonsterEntity::canMonsterSpawn);
+        });
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
